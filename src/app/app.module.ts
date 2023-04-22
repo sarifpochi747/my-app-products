bd052bdc6ecf7aee8532d064e3a2431b2dadd6f6
sarif pochi <sarifpochi747@gmail.com>
2023-04-22 21:17:54 +0700

    c3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/products.ts
+++
@@ -1,8 +0,0 @@
-
-export const products = [
-    {
-        name : "sarif",
-        price : 700,
-        description : "aaaa"
-    }
-]
