51af5420957ec26789a536d35fd01c39e036f643
sarif pochi <sarifpochi747@gmail.com>
2023-04-23 01:35:26 +0700

    c3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './Header/header.component';
+import { LoginComponent } from './login/login.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    HeroSectionComponent,
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ 
+    content: [
+      "./src/**/*.{html,ts}",
+    ],
+  theme: {
+    extend: {
+      colors:{
+        'white-1':"#FAF9F7",
+        "red-1":"#A61F23",
+        "grey-1":"#282828",
+      },
+    },
+  },
+  plugins: [],
+}
+
