2e8a620a1722e7a7f4d580ceb76c12d226fbd23a
sarif pochi <sarifpochi747@gmail.com>
2023-04-23 18:51:08 +0700

    c3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
-import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { PurposeComponent } from './purpose/purpose.component';
-
+import { HeroSectionComponent } from './home_page/hero-section/hero-section.component';
+import { PurposeComponent } from './home_page/purpose/purpose.component';
+import { StandardComponent } from './home_page/standard/standard.component';
+import { QuoteComponent } from './home_page/quote/quote.component';
+import { UserSubmitComponent } from './home_page/user-submit/user-submit.component';
+import { HomeComponent } from './home_page/home.component';
+const routes: Routes = [
+  { path: 'home', component: HeaderComponent },
+]; 
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,13 +22,19 @@ import { PurposeComponent } from './purpose/purpose.component';
     LoginComponent,
     HeroSectionComponent,
     PurposeComponent,
+    StandardComponent,
+    QuoteComponent,
+    UserSubmitComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    [RouterModule.forRoot(routes)]
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 
 export class AppModule { }
