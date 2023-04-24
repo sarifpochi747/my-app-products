17de9a2231d6ffba7a8f8ca5b013e41c7a27a115
sarif pochi <sarifpochi747@gmail.com>
2023-04-24 14:28:32 +0700

    c3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login_page/login.component';
 import { HeroSectionComponent } from './home_page/hero-section/hero-section.component';
 import { PurposeComponent } from './home_page/purpose/purpose.component';
 import { StandardComponent } from './home_page/standard/standard.component';
 import { QuoteComponent } from './home_page/quote/quote.component';
 import { UserSubmitComponent } from './home_page/user-submit/user-submit.component';
 import { HomeComponent } from './home_page/home.component';
+import { MenuPageComponent } from './menu_page/menu-page.component';
+
+
 const routes: Routes = [
-  { path: 'home', component: HeaderComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'menu', component: MenuPageComponent },
 ]; 
 @NgModule({
   declarations: [
@@ -26,7 +31,9 @@ const routes: Routes = [
     QuoteComponent,
     UserSubmitComponent,
     HomeComponent,
+    MenuPageComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
